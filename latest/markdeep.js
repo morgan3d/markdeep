8751125b113891ea06076556e06f78af13ed8ecb
Morgan <morgan3d@gmail.com>
2020-08-24 22:10:49 -0400

    archive

--- latest/markdeep.js
+++ latest/markdeep.js
@@ -94,6 +94,36 @@ function measureFontSize(fontStack) {
     }
 }
 
+// IE11 polyfill needed by Highlight.js, from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+if (typeof Object.assign !== 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) { // .length of function is 2
+            if (target === null || target === undefined) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+            
+            var to = Object(target);
+            
+            for (var index = 1; index < arguments.length; index++) {
+                var nextSource = arguments[index];
+                
+                if (nextSource !== null && nextSource !== undefined) { 
+                    for (var nextKey in nextSource) {
+                        // Avoid bugs when hasOwnProperty is shadowed
+                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        },
+        writable: true,
+        configurable: true
+    });
+}
+    
 /*
   Highlight.js 10.0.0-beta.0 (ca23180c)
   License: BSD-3-Clause
